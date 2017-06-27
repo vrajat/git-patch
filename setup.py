27248d4a09da92e8b924d09f28a02411c3d8641a
Rajat Venkatesh <rvenkatesh@qubole.com>
2017-06-27 15:53:16 +0530

    chg:usr:Upgrade version to 2.1.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='git_patch',
-    version='2.0.1',
+    version='2.1.0',
     description='Patch management for open source fork projects',
 
     author='Rajat Venkatesh',
